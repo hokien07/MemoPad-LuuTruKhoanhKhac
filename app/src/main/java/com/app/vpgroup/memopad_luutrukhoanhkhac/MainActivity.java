174704872dc7ad704a7c61336594ad6b567b9ff0
HoKien <hokien07@gmail.com>
2016-12-06 07:23:20 +0700

    Do an android

---
+++ app/src/main/java/com/app/vpgroup/memopad_luutrukhoanhkhac/MainActivity.java
@@ -0,0 +1,13 @@
+package com.app.vpgroup.memopad_luutrukhoanhkhac;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
